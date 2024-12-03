77c58f31d3b555723c5d1bb9f5537bf86c7326be
Ama143 <70701729+Ama143@users.noreply.github.com>
2024-12-03 17:48:56 +0545

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def download_video_section(url, start_time, end_time, output_file):
         'format': '(bestvideo+bestaudio/best)[height>=?2160][fps>=?60]/(bestvideo+bestaudio/best)[height>=?1440][fps>=?60]/(bestvideo+bestaudio/best)[height>=?1080][fps>=?60]/bestvideo+bestaudio/best',
         'outtmpl': output_file,
         'cookies': './cookies.txt',
+        'cookies-from-browser':'chrome',
         'download_ranges': download_section(start_time, end_time),
         'merge_output_format': 'mp4',
         'postprocessors': [{
