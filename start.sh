0e157171f6955776915b5e0d96a2d0b1a766cc43
Ama143 <70701729+Ama143@users.noreply.github.com>
2024-12-05 16:01:12 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ export ENV_VAR=value
 
 # Change file permissions for the cookies file
 chmod 644 /opt/render/project/src/cookies.txt
-
+chmod 600 cookies.txt
 # Download video using yt-dlp with the correct cookies file path
 yt-dlp --cookies /opt/render/project/src/cookies.txt https://youtube.com/shorts/zToQkPR4PEg?si=Jf08HN2fzA-goctq
 
