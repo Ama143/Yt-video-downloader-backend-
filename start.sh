0d462695d09bdcb3568b8db90f4e4572e984f3d9
Ama143 <70701729+Ama143@users.noreply.github.com>
2024-12-05 17:38:32 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ export ENV_VAR=value
 chmod 644 /opt/render/project/src/cookies.txt
 chmod 600 cookies.txt
 # Download video using yt-dlp with the correct cookies file path
-yt-dlp --cookies /opt/render/project/src/cookies.txt https://youtube.com/shorts/zToQkPR4PEg?si=Jf08HN2fzA-goctq
+yt-dlp --cookies ./cookies.txt https://youtube.com/shorts/zToQkPR4PEg?si=Jf08HN2fzA-goctq
 
 # Start the Gunicorn server for your app
 gunicorn app:app
